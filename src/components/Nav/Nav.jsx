c12d6313fb0f02004a43c1392743a3ad0db82e76
xiongster94 <xiongster94@users.noreply.github.com>
2021-06-29 08:48:57 -0500

    made reader.router and added GET and POST route functionality

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -20,7 +20,7 @@ function Nav() {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <h2 className="nav-title">Read-Star</h2>
       </Link>
       <div>
         <Link className="navLink" to={loginLinkData.path}>
@@ -29,8 +29,8 @@ function Nav() {
 
         {user.id && (
           <>
-            <Link className="navLink" to="/info">
-              Info Page
+            <Link className="navLink" to="/addReader">
+              Add Reader
             </Link>
             <LogOutButton className="navLink" />
           </>
