6db6963def7d13df6a0e837c0be3bd28fa78a296
xiongster94 <xiongster94@users.noreply.github.com>
2021-06-29 09:56:08 -0500

    made form value for addReader feature

--- src/components/AddReader/AddReader.jsx
+++ src/components/AddReader/AddReader.jsx
@@ -1,14 +1,85 @@
 import React from 'react';
 
-// This is one of our simplest components
-// It doesn't have local state
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function AddReader() {
+
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [goal, setGoal] = useState(0);
+  const [reward, setReward] = useState('');
+
+  const handleChangeName = (event) => {
+      setName(event.target.value)
+  }
+  
+  const handleChangeGoal = (event) => {
+    setGoal(event.target.value)
+  }
+
+  const handleChangeReward = (event) => {
+    setReward(event.target.value)
+  }
+
+
+  const handleSubmit = event => {
+      event.preventDefault()
+
+      const newReader = {
+        name: name,
+        goal: goal,
+        reward: reward
+      }
+    
+      dispatch({
+          type: 'SET_READERS',
+          payload: newReader
+      })
+      setName('');
+    }
+
   return (
     <div className="container">
       <p>Add Reader</p>
+      
+      <form onSubmit={handleSubmit}>
+        
+        <TextField 
+        id="outlined-basic"
+        label="New Racer's Name"
+        variant="outlined"
+        value={name}
+        onChange={handleChangeName}
+        required
+        />
+
+        <TextField
+          id="filled-number"
+          label="Goal (Books To Read)"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="filled"
+          onChange={handleChangeGoal}
+          required
+        />
+
+        <TextField 
+        id="outlined-basic"
+        label="Choose a Prize"
+        variant="outlined"
+        value={reward}
+        onChange={handleChangeReward}
+        required
+        />
+
+        <Button type="submit" variant="outlined">Add Read-Star!</Button>
+        </form>
+
     </div>
   );
 }
