3b4afbdc23388e7ec2f91d72aef2201f1f8f63d7
xiongster94 <xiongster94@users.noreply.github.com>
2021-07-01 16:16:15 -0500

    got popup to funcfunction

--- src/components/ReaderTrack/ReaderTrack.jsx
+++ src/components/ReaderTrack/ReaderTrack.jsx
@@ -1,23 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
+import EditReaderForm from '../EditReaderForm/EditReaderForm';
 
-function ReaderTrack () {
+function ReaderTrack() {
+
+    // edit button functionality
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const toggleEditForm = () => {
+        setIsPopupOpen(!isPopupOpen);
+    }
 
     const selectedReader = useSelector((store) => store.selectedReader)
     console.log('reader info', selectedReader)
 
     return (
         <div className="container">
-        <h2>{selectedReader[0]?.reader_name}'s Track</h2>
+            <h2>{selectedReader[0]?.reader_name}'s Track</h2>
 
-        <h3>Goal: {selectedReader[0]?.goal}</h3>
-        <h3>Reward: {selectedReader[0]?.reward}</h3>
-        
-        <Button onClick={} variant="outlined">Edit</Button>
-      </div>
+            <h3>Goal: {selectedReader[0]?.goal}</h3>
+            <h3>Reward: {selectedReader[0]?.reward}</h3>
+
+            <main>
+                <input
+                    type="button"
+                    value="Click to Open Popup"
+                    onClick={toggleEditForm}
+                />
+                {isPopupOpen && <EditReaderForm
+                    content={<>
+                        <b>Edit Reader Stats</b>
+                        <p>Test</p>
+                        <Button>Submit Changes</Button>
+                    </>}
+                    handleClose={toggleEditForm}
+                />}
+            </main>
+
+        </div>
     )
 }
 
