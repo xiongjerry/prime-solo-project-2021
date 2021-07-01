ee5d8e6762bb4c69fd5dd1040128656649840ee9
xiongster94 <xiongster94@users.noreply.github.com>
2021-07-01 10:33:33 -0500

    got reader specciific into to display

--- src/components/ReaderTrack/ReaderTrack.jsx
+++ src/components/ReaderTrack/ReaderTrack.jsx
@@ -1,8 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+
 function ReaderTrack () {
+
+    const selectedReader = useSelector((store) => store.selectedReader)
+    console.log('reader info', selectedReader)
+
     return (
-        <>
-            <p>IN READER TRACK</p>
-        </>
+        <div className="container">
+        <h2>{selectedReader[0]?.reader_name}'s Track</h2>
+
+        <h3>Goal: {selectedReader[0]?.goal}</h3>
+        <h3>Reward: {selectedReader[0]?.reward}</h3>
+  
+      </div>
     )
 }
 
