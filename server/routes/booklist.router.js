d663d46c439dda7815ae2e7534bcca086db9c383
xiongster94 <xiongster94@users.noreply.github.com>
2021-07-02 08:00:20 -0500

    made a router for booklist

---
+++ server/routes/booklist.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+// Booklist GET route
+router.get('/', (req, res) => {
+  // GET route code here
+});
+
+// Booklist POST route
+router.post('/', (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const readerRouter = require('./routes/reader.router');
+const booklistRouter = require('./routes/booklist.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,8 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/reader', readerRouter)
+app.use('/api/booklist', booklistRouter)
+
 // Serve static files
 app.use(express.static('build'));
 
