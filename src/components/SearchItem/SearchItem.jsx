09827b8b2b2065a2ef8e6e82aca610d4d1115705
xiongster94 <xiongster94@users.noreply.github.com>
2021-07-02 12:02:34 -0500

    added dispatch and button for book saga in search item

--- src/components/SearchItem/SearchItem.jsx
+++ src/components/SearchItem/SearchItem.jsx
@@ -1,9 +1,18 @@
+import { useDispatch } from "react-redux";
+import Button from "@material-ui/core/Button";
+
 function SearchItem( { book }) {
+
+    const dispatch = useDispatch();
 
     const title = book.volumeInfo.title;
     const author = book.volumeInfo.authors
     let bookUrl = book.volumeInfo.imageLinks.thumbnail;
     
+    const handleClick = () => {
+        dispatch({type: 'ADD_BOOK', payload: book})
+    }
+
 return (
   
       <div>
@@ -11,6 +20,8 @@ return (
         <p> {book.volumeInfo.subtitle}</p>
         <p> Author: {author}</p>
         <img src={bookUrl} alt="searched Book results" width="300px"></img>
+
+        <Button variant="contained" onClick={handleClick}>Add to List</Button>
       </div>
 
   );
