b868cf8692205d6c5e5275226994b0e8c30d181b
xiongster94 <xiongster94@users.noreply.github.com>
2021-07-02 11:57:07 -0500

    made some minor adjustments to bookUrl in searchItem

--- src/components/SearchItem/SearchItem.jsx
+++ src/components/SearchItem/SearchItem.jsx
@@ -1,15 +1,16 @@
 function SearchItem( { book }) {
 
     const title = book.volumeInfo.title;
-    const bookUrl = book.volumeInfo.imageLinks.thumbnail
-
+    const author = book.volumeInfo.authors
+    let bookUrl = book.volumeInfo.imageLinks.thumbnail;
     
 return (
   
       <div>
         <p> Title: {title}</p>
-        <p> {book.volumeInfo.subtitle} </p>
-        <img src={bookUrl} alt="searched Book results" width="360px"></img>
+        <p> {book.volumeInfo.subtitle}</p>
+        <p> Author: {author}</p>
+        <img src={bookUrl} alt="searched Book results" width="300px"></img>
       </div>
 
   );
