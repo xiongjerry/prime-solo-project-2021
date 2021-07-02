52b188ffa40e1fd0c1d576c9fa084c650f52925f
xiongster94 <xiongster94@users.noreply.github.com>
2021-07-02 09:26:50 -0500

    added input form am and button for search bar in book search

--- src/components/BookSearch/BookSearch.jsx
+++ src/components/BookSearch/BookSearch.jsx
@@ -1,8 +1,28 @@
-function BookSearch () {
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+function BookSearch() {
+
+    const [searchQuery, setSearchQuery] = useState('')
+
+    const handleChange = (event) => {
+        setSearchQuery(event.target.value)
+    }
+
+
     return (
-        <>
-        <h1>Book Search</h1>
-        </>
+        <div className="container">
+            <h1>Book Search</h1>
+
+
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleChange} value={searchQuery} lable="Search for books" />
+                <Button type="submit" variant="outlined">Submit Search</Button>
+            </form>
+        </div>
     )
 }
 
