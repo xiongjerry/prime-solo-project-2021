7348e2189d6b1b4d064c0e2233776483c31986bb
xiongster94 <xiongster94@users.noreply.github.com>
2021-07-07 22:19:58 -0500

    made delete route for booklist router

--- server/routes/booklist.router.js
+++ server/routes/booklist.router.js
@@ -38,4 +38,14 @@ router.post('/', rejectUnauthenticated,(req, res) => {
     });
 });
 
+//Booklist Delete Route
+router.delete('/:id', (req, res) => {
+  pool.query('DELETE FROM "books_list" WHERE id=$1', [req.params.id]).then((result) => {
+      res.sendStatus(200);
+  }).catch((error) => {
+      console.log('Error DELETE /api/booklist', error);
+      res.sendStatus(500);
+  })
+});
+
 module.exports = router;
