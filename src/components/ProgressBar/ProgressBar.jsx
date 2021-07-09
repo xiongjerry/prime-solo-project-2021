25be13b54dcc70418c30a0792d65a8a8e1fdeac5
xiongster94 <xiongster94@users.noreply.github.com>
2021-07-09 06:42:10 -0500

    added styling to progress bar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,18 +1,43 @@
-import './ProgressBar.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const ProgressBar = ({ value, max }) => {
+
+    const containerStyles = {
+        height: 20,
+        width: '30%',
+        backgroundColor: "#e0e0de",
+        borderRadius: 50,
+        margin: 50
+    }
+
+    const fillerStyles = {
+        height: '100%',
+        width: `${max}%`,
+        backgroundColor: "#78909c",
+        borderRadius: 'inherit',
+        textAlign: 'right'
+    }
+
+    const labelStyles = {
+        padding: 5,
+        color: 'white',
+        fontWeight: 'bold'
+    }
+
     return (
-        <>
-            <progress value={value} max={max} />
-        </>
+        <div style={containerStyles}>
+            <div style={fillerStyles}>
+                <progress value={value} max={max} />
+                <span style={labelStyles}> {value} / {max} </span>
+            </div>
+        </div>
     )
 };
 
 ProgressBar.PropTypes = {
     value: PropTypes.number.isRequired,
-    max : PropTypes.number
+    max: PropTypes.number
 };
 
 ProgressBar.defaultProps = {
